011307a81938605f5ba7736336091b4bdaccd8fb
Pavel Sheshko <psheshko@twelvedevs.com>
2023-06-25 12:30:54 +0300

    feat: table with file extension and type

--- src/functions/lsFunction.js
+++ src/functions/lsFunction.js
@@ -1,11 +1,18 @@
-import { resolve } from 'node:path'
+import { resolve, extname } from 'node:path'
 import { readdir } from 'node:fs/promises'
 
 export const  lsFunction = async () => {
     try{
         const currentDirectory = resolve(process.cwd());
-        const files = await readdir(currentDirectory);
-        console.table(files);
+        const files = await readdir(currentDirectory, { withFileTypes: true });
+
+        const filesToTable = files.map(file => {
+            if (file.isFile()) return {Name: file.name, Type: 'file'}
+            if (file.isDirectory) return {Name: file.name, Type: 'directory'}
+            return {Name: file.name, Type: 'unknown'}
+        })
+
+        console.table(filesToTable);
         console.log('You are currently in ', process.cwd());
     } catch (err) {
         console.error('Operation failed');
